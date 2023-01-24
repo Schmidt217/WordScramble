d9bc647ca26f189ac06113a4668dd8cfdecfcb82
Mike Schmidt <schmidt217@gmail.com>
2023-01-24 15:23:03 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Michael Schmidt on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
